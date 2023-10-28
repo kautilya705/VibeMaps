0995608789b578543aa93a3c0228d335689ad6eb
rohseh303 <rohansehgal935@gmail.com>
2023-10-27 20:47:58 -0700

    need to fix convex changes

--- MusicMap/App.tsx
+++ MusicMap/App.tsx
@@ -2,15 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Auth from "./Screens/Auth";
 import Navbar from "./Components/Navbar";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import "react-native-get-random-values";
+import { CONVEX_URL } from "@env";
+import Tasks from "./Tasks";
+
+const convex = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Auth />
-      <Navbar />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Auth />
+        <Navbar />
+      </View>
+
+      <ConvexProvider client={convex}>
+        <Tasks />
+      </ConvexProvider>
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
